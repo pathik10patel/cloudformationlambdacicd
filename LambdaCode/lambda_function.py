120686eed5aab546e2582c3c68dcf2a60f3eecaf
pathik10patel <pathik10patel@gmail.com>
2020-03-14 15:03:51 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,8 +1,20 @@
-import boto3
-import json
+#import boto3
+#import json
 
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+#ec2 = boto3.client('ec2')
+#def lambda_handler(event, context):
+#    response = ec2.describe_availability_zones()
+#    return {"statusCode": 200, "body": json.dumps(response)}
 #hello hi
+import json
+import bs4 as bs
+from urllib import request
+def lambda_handler(event, context):
+    # TODO implement
+    html_data = request.urlopen("https://pythonprogramming.net/introduction-scraping-parsing-beautiful-soup-tutorial/").read()
+    soup = bs.BeautifulSoup(html_data,'lxml')
+    title = soup.find('h2')
+    return {
+        "statusCode": 200,
+        "body": json.dumps(title.text)
+    }
